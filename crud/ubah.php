c7198467d3616f20a113194b94e728a8b82d4895
yoginarbi <89401994+yoginarbi@users.noreply.github.com>
2025-02-19 21:21:59 +0700

    Update ubah.php
    Update Ubah

--- crud/ubah.php
+++ crud/ubah.php
@@ -6,11 +6,17 @@ if (!isset($_SESSION["login"])) {
 }
 require 'functions.php';
 
-$id = $_GET["id"];
-
-$mhs = query("SELECT * FROM mahasiswa WHERE id = '$id'")[0];
+$id = intval($_GET["id"]); // Pastikan id adalah angka
+$stmt = $conn->prepare("SELECT * FROM mahasiswa WHERE id = ?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$result = $stmt->get_result();
+$mhs = $result->fetch_assoc();
 
 if (isset($_POST["submit"])) {
+    if (!isset($_POST["csrf_token"]) || $_POST["csrf_token"] !== $_SESSION["csrf_token"]) {
+        die("CSRF token tidak valid");
+    }
     if (ubah($_POST) > 0) {
         echo '
             <script>
@@ -27,6 +33,7 @@ if (isset($_POST["submit"])) {
         ';
     }
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,30 +47,34 @@ if (isset($_POST["submit"])) {
 
 <body>
     <h1>Ubah Data Mahasiswa</h1>
+    if (!isset($_SESSION["csrf_token"])) {
+        $_SESSION["csrf_token"] = bin2hex(random_bytes(32));
+    }
     <form action="" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="csrf_token" value="<?= $_SESSION["csrf_token"]; ?>">
         <input type="hidden" name="id" value="<?= $mhs["id"]; ?>">
         <input type="hidden" name="gambarLama" value="<?= $mhs["gambar"]; ?>">
         <ul>
             <li class="jarak">
                 <label for="nama">Nama : </label>
-                <input type="text" name="nama" id="nama" value="<?= $mhs["nama"]; ?>">
+                <input type="text" name="nama" id="nama" value="<?= htmlspecialchars($mhs["nama"]); ?>">
             </li>
             <li class="jarak">
                 <label for="nrp">NRP : </label>
-                <input type="number" name="nrp" id="nrp" value="<?= $mhs["nrp"]; ?>">
+                <input type="number" name="nrp" id="nrp" value="<?= htmlspecialchars($mhs["nrp"]); ?>">
             </li>
             <li class="jarak">
                 <label for="email">E-mail : </label>
-                <input type="text" name="email" id="email" value="<?= $mhs["email"]; ?>">
+                <input type="text" name="email" id="email" value="<?= htmlspecialchars($mhs["email"]); ?>">
             </li>
             <li class="jarak">
                 <label for="jurusan">Jurusan : </label>
-                <input type="text" name="jurusan" id="jurusan" value="<?= $mhs["jurusan"]; ?>">
+                <input type="text" name="jurusan" id="jurusan" value="<?= htmlspecialchars($mhs["jurusan"]); ?>">
             </li>
             <li class="jarak">
                 <label for="gambar">Gambar : </label><br>
                 <img src="img/<?= $mhs["gambar"]; ?>" width="40"><br>
-                <input type="file" name="gambar" id="gambar">
+                <input type="file" name="gambar" id="gambar" accept=".jpg, .jpeg, .png">
             </li>
         </ul>
         <button type="submit" name="submit">Ubah Data</button>
