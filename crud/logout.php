26207683b45766a08ff7d18d280dca460916e0b7
yoginarbi <89401994+yoginarbi@users.noreply.github.com>
2025-02-19 20:49:50 +0700

    Update logout.php
    Update Logout

--- crud/logout.php
+++ crud/logout.php
@@ -4,8 +4,9 @@ $_SESSION = [];
 session_unset();
 session_destroy();
 
-setcookie('id', '', time() - 3600);
-setcookie('key', '', time() - 3600);
+// Menghapus cookie dengan opsi yang lebih aman
+setcookie('id', '', time() - 3600, '/', '', true, true);
+setcookie('key', '', time() - 3600, '/', '', true, true);
 
 header("Location: login.php");
 exit;
